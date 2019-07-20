e5ae55d558055a65d2c3c7e58aa1e52c28fca47b
aiyuyun2015 <alvin2009@foxmail.com>
2019-07-20 18:48:31 -0400

    add my files

--- ch18/mtsleep2.py
+++ ch18/mtsleep2.py
@@ -9,22 +9,29 @@ def loop(nloop, nsec, lock):
     print 'start loop', nloop, 'at:', ctime()
     sleep(nsec)
     print 'loop', nloop, 'done at:', ctime()
-    lock.release()
+    lock.release()  ###-> indicate the main function this thread is completed.
 
 def main():
     print 'starting threads...'
     locks = []
     nloops = range(len(loops))
 
+    print("nloops:{}".format(nloops))
+
+    #init two locks and append them to a list called locks
     for i in nloops:
-        lock = thread.allocate_lock()
-        lock.acquire()
+        lock = thread.allocate_lock()   ##init lock object.
+        ##in fact, acquire the lock
+        lock.acquire() 
         locks.append(lock)
 
+    #spawn thread with arguments, kwargs, the arguments is tuple, which set the parameters in the loop function
     for i in nloops:
         thread.start_new_thread(loop, 
             (i, loops[i], locks[i]))
 
+    #check the lock status sequencially
+    
     for i in nloops:
         while locks[i].locked(): pass
 
--- ch18/mtsleep3.py
+++ ch18/mtsleep3.py
@@ -15,11 +15,14 @@ def main():
     threads = []
     nloops = range(len(loops))
 
+    #when instantiate the Thread, it doesn't start immediately. Instead, wait, until call start()
     for i in nloops:
         t = threading.Thread(target=loop,
 	    args=(i, loops[i]))
         threads.append(t)
 
+
+        
     for i in nloops:            # start threads
         threads[i].start()
 
